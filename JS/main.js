efd81e8aa63e58a07f702eaa66d933f7c02f9343
Khanh <pvkhanh.cv@gmail.com>
2022-01-13 21:17:01 +0700

    fix search

--- JS/main.js
+++ JS/main.js
@@ -26,12 +26,40 @@ function renderData(arr){
     document.getElementById('posts').innerHTML = post
 }
 
+function renderFilterData(arr){
+    let post = ``
+    arr.map(value => {
+        post += 
+        `<div class="card">
+            <div class="card_img">
+                <img src="${value.image}" alt="">
+            </div>
+            <div class="card_title">
+                ${value.name}
+            </div>
+            <div class="card_price">
+                ${value.priceDisplay} VND/Kg
+            </div>
+            <div class="card_action">
+                <input type="number" min="1">
+                <button>Thêm vào giỏ</button>
+            </div>
+        </div>`
+    })
+    document.getElementById('posts').innerHTML = post
+}
+
 function searchData(){
     let valueSearch = document.getElementById('search').value
     const dataFilter = data.filter(value => {
         return value.name.toUpperCase().includes(valueSearch.toUpperCase())
     })
-    renderData(dataFilter)
+    if (valueSearch == '') {
+        renderData(perPost)
+    }
+    else
+        renderFilterData(dataFilter)
+    console.log(valueSearch)
 }
 
 function handlePage(key){
